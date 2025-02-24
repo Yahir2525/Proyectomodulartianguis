e4bec6f9dbe76dcff8c74033d2c3a1858a74d506
Juan Jose Marquez Ponce <juan.marquez0213@alumnos.udg.mx>
2025-02-23 18:07:02 -0600

    Producto controller

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -12,6 +12,9 @@ class ProductoController extends Controller
      */
     public function index()
     {
+        $producto = new Producto();
+        $productoIndex = Producto::all();
+        return view('producto/productoIndex', compact ('productoIndex'));
         //
     }
 
@@ -20,6 +23,7 @@ class ProductoController extends Controller
      */
     public function create()
     {
+        return view('producto/createProducto');
         //
     }
 
@@ -28,6 +32,62 @@ class ProductoController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'nombre' => 'required|string|max:100',
+            'material' => 'requiered|string|max:20',
+            'color'=> 'requiered|string|max:20',
+            'tamanio' => 'required|string|max:25',
+            'marca' => 'required|string|max:25',
+            'precio_unitario' => 'required|decimal|max:30',
+            'piezas' => 'required|integer',
+        ], [
+            'nombre.required' => 'El nombre es obligatorio.',
+            'nombre.string' => 'El nombre debe ser una cadena de texto.',
+            'nombre.max' => 'El nombre no puede tener más de 100 caracteres.',
+            'material.required' => 'El material es obligatorio.',
+            'material.string' => 'El material debe ser una cadena de texto.',
+            'material.max' => 'El material no puede tener mas de 20 caracteres.',
+            'color.required' => 'El color es obligatorio.',
+            'color.string' => 'El color debe ser una cadena de caracteres.',
+            'color.max' => 'El color no puede tener mas de 20 caracteres.',
+            'tamanio.required' => 'El tamaño es obligatorio.',
+            'tamanio.string' => 'El tamaño debe ser una cadena de caracteres.',
+            'tamanio.min' => 'El tamaño debe tener maximo 25 caracteres.',
+            'marca.required' => 'La marca es obligatoria.',
+            'marca.string' => 'La marca debe ser una cadena de caracteres.',
+            'marca.max' => 'La marca no puede tener más de 25 caracteres.',
+            'precio_unitario.required' => 'El precio es obligatorio.',
+            'precio_unitario.string' => 'El precio debe de ser un numero.',
+            'precio_unitario' => 'El precio debe de tener maximo 30 caracteres.',
+            'piezas.required' => 'El nombre de la pieza es obligatorio.',
+            'piezas.integer' => '',
+            
+        ]);
+            // $request->validate([
+            //     'direccion' => 'required|string|max:255',
+            //     'telefono' => 'required|string|unique:clientes',
+            //     'comentario' => 'nullable|string|max:255',
+            // ], [
+            //     'direccion.required' => 'El campo dirección es obligatorio.',
+            //     'direccion.string' => 'El campo dirección debe ser una cadena de texto.',
+            //     'direccion.max' => 'El campo dirección no puede tener más de 255 caracteres.',
+            //     'telefono.required' => 'El campo teléfono es obligatorio.',
+            //     'telefono.string' => 'El campo teléfono debe ser una cadena de texto.',
+            //     'telefono.unique' => 'El teléfono ya está en uso.',
+            //     'comentario.string' => 'El campo comentario debe ser una cadena de texto.',
+            //     'comentario.max' => 'El comentario no puede tener más de 255 caracteres.'
+            // ]);
+            $producto = new Producto();
+            $producto->nombre = $request->nombre;
+            $producto->material = $request->material;
+            $producto->color = $request->color;
+            $producto->tamanio = $request->tamanio;
+            $producto->marca = $request->marca;
+            $producto->precio_unitario = $request->precio_unitario;
+            $producto->piezas = $request->piezas;
+            
+            $producto->save();
+        return redirect('/producto')->with('success', 'Producto registrado correctamente.');
         //
     }
 
@@ -36,6 +96,13 @@ class ProductoController extends Controller
      */
     public function show(Producto $producto)
     {
+        $id = $request->input('id_producto');
+        $producto = Producto::find($id);
+            
+        if (!$producto) {
+            return redirect()->back()->with('error', 'El producto no se encontró.');
+        }
+        return view('/abono/showProducto', ['producto' => $producto]);
         //
     }
 
@@ -44,6 +111,14 @@ class ProductoController extends Controller
      */
     public function edit(Producto $producto)
     {
+        $producto = Producto::find($id);
+
+            if (!$producto) {
+                return redirect()->route('producto.productoIndex')->with('error', 'El producto no se encontró.');
+            // }
+
+            return view('/producto/editProducto', ['producto' => $producto]);   
+        }
         //
     }
 
@@ -52,6 +127,52 @@ class ProductoController extends Controller
      */
     public function update(Request $request, Producto $producto)
     {
+        $request->validate([
+            'nombre' => 'required|string|max:100',
+            'material' => 'requiered|string|max:20',
+            'color'=> 'requiered|string|max:20',
+            'tamanio' => 'required|string|max:25',
+            'marca' => 'required|string|max:25',
+            'precio_unitario' => 'required|decimal|max:30',
+            'piezas' => 'required|integer',
+        ], [
+            'nombre.required' => 'El nombre es obligatorio.',
+            'nombre.string' => 'El nombre debe ser una cadena de texto.',
+            'nombre.max' => 'El nombre no puede tener más de 100 caracteres.',
+            'material.required' => 'El material es obligatorio.',
+            'material.string' => 'El material debe ser una cadena de texto.',
+            'material.max' => 'El material no puede tener mas de 20 caracteres.',
+            'color.required' => 'El color es obligatorio.',
+            'color.string' => 'El color debe ser una cadena de caracteres.',
+            'color.max' => 'El color no puede tener mas de 20 caracteres.',
+            'tamanio.required' => 'El tamaño es obligatorio.',
+            'tamanio.string' => 'El tamaño debe ser una cadena de caracteres.',
+            'tamanio.min' => 'El tamaño debe tener maximo 25 caracteres.',
+            'marca.required' => 'La marca es obligatoria.',
+            'marca.string' => 'La marca debe ser una cadena de caracteres.',
+            'marca.max' => 'La marca no puede tener más de 25 caracteres.',
+            'precio_unitario.required' => 'El precio es obligatorio.',
+            'precio_unitario.string' => 'El precio debe de ser un numero.',
+            'precio_unitario' => 'El precio debe de tener maximo 30 caracteres.',
+            'piezas.required' => 'El nombre de la pieza es obligatorio.',
+            'piezas.integer' => '',
+            
+        ]);
+        $producto = Producto::find($id);
+    
+        if (!$producto) {
+            return redirect()->route('producto.productoIndex')->with('error', 'El producto no se encontró.');
+        }
+        $producto = new Producto();
+        $producto->nombre = $request->nombre;
+        $producto->material = $request->material;
+        $producto->color = $request->color;
+        $producto->tamanio = $request->tamanio;
+        $producto->marca = $request->marca;
+        $producto->precio_unitario = $request->precio_unitario;
+        $producto->piezas = $request->piezas;
+        $producto->save();
+        return redirect()->route('producto.productoIndex')->with('success', 'El producto se ha actualizado con éxito.');
         //
     }
 
@@ -60,6 +181,17 @@ class ProductoController extends Controller
      */
     public function destroy(Producto $producto)
     {
+        $producto = Producto::find($id);
+
+
+        if (!$producto) {
+            return redirect()->route('producto.productoIndex')->with('error', 'El producto no se encontró.');
+        }
+        
+
+        $cliente->delete();
+
+        return redirect()->route('producto.productoIndex')->with('success', 'El producto se ha eliminado con éxito.');
         //
     }
 }
--- app/Http/Controllers/VendedorController.php
+++ app/Http/Controllers/VendedorController.php
@@ -12,6 +12,9 @@ class VendedorController extends Controller
      */
     public function index()
     {
+        $vendedor = new Vendedor();
+        $vendedorIndex = Vendedor::all();
+        return view('vendedor/vendedorIndex', compact ('vendedorIndedx'));
         //
     }
 
@@ -20,6 +23,7 @@ class VendedorController extends Controller
      */
     public function create()
     {
+        return view('vendedor/createVendedor');
         //
     }
 
@@ -28,6 +32,53 @@ class VendedorController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'nombre' => 'required|string|max:100',
+            'genero' => 'requiered|character|min:1',
+            'edad'=> 'requiered|integer|min:0|max:120',
+            'telefono' => 'required|integer|min:10|unique:vendedors',
+            'direccion' => 'required|string|max:255',
+            'correo' => 'required|string|email|max:30|unique:vendedors',
+            'nombre_usuario' => 'required|string|max:30|unique:vendedors',
+        ], [
+            'nombre.required' => 'El nombre es obligatorio.',
+            'nombre.string' => 'El nombre debe ser una cadena de texto.',
+            'nombre.max' => 'El nombre no puede tener más de 100 caracteres.',
+            'genero.required' => 'El genero es obligatorio.',
+            'genero.character' => 'El genero debe ser un caracter.',
+            'genero.min' => 'El genero debe ser al menos 1.',
+            'edad.required' => 'La edad es obligatoria.',
+            'edad.integer' => 'La edad debe ser un entero.',
+            'edad.min' => 'La edad debe ser minimo 0.',
+            'edad.max' => 'La edad debe ser máximo 120 años.',
+            'telefono.required' => 'El teléfono es obligatorio.',
+            'telefono.integer' => 'El teléfono debe ser un entero.',
+            'telefono.min' => 'El teléfono debe tener al menos diez digitos.',
+            'telefono.unique' => 'El teléfono ya está en uso.',
+            'direccion.required' => 'La dirección es obligatorio.',
+            'direccion.string' => 'La dirección debe ser una cadena de texto.',
+            'direccion.max' => 'La dirección no puede tener más de 255 caracteres.',
+            'correo.required' => 'El correo electrónico es obligatorio.',
+            'correo.string' => 'El correo electrónico debe ser una cadena de texto.',
+            'correo.email' => 'El correo electrónico no es válido.',
+            'correo.unique' => 'El correo electrónico ya está en uso.',
+            'correo.max' => 'El correo electrónico no puede tener más de 30 caracteres.',
+            'nombre_usuario.required' => 'El nombre de usuario es obligatorio.',
+            'nombre_usuario.string' => 'El nombre de usuario debe ser una cadena de texto.',
+            'nombre_usuario.max' => 'El nombre de usuario no puede tener más de 30 caracteres.',
+            'nombre_usuario.unique' => 'El nombre de usuario ya está en uso.',
+        ]);
+            $vendedor = new Cliente();
+            $vendedor->nombre = $request->nombre;
+            $vendedor->genero = $request->genero;
+            $vendedor->edad = $request->edad;
+            $vendedor->telefono = $request->telefono;
+            $vendeor->direccion = $request->direccion;
+            $vendedor->correo = $request->correo;
+            $vendedor->nombre_usuario = $request->nombre_usuario;
+            
+            $vendedor->save();
+            return redirect('/vendedor')->with('success', 'Vendedor registrado correctamente.');
         //
     }
 
@@ -36,6 +87,13 @@ class VendedorController extends Controller
      */
     public function show(Vendedor $vendedor)
     {
+        $id = $request->input('id_vendedor');
+        $cliente = Cliente::find($id);
+            
+        if (!$cliente) {
+            return redirect()->back()->with('error', 'El cliente no se encontró.');
+        }
+        return view('/abono/showCliente', ['cliente' => $cliente]);
         //
     }
 
